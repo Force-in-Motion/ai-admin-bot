8d444a79e5215e4cde732c8d3b18db0fc181009c
Force-in-Motion <canzdao@gmail.com>
2026-02-09 16:49:36 +0300

    feat (models): add Appointment model

---
+++ app/database/models/appointment.py
@@ -0,0 +1,63 @@
+from typing import TYPE_CHECKING
+from datetime import datetime
+from sqlalchemy import ForeignKey, DateTime, Enum, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.database.models.base import Base
+from app.tools.types import AppointmentStatus
+
+
+if TYPE_CHECKING:
+    from app.database.models import Client, Executor
+
+
+class Appointment(Base):
+
+    __tablename__ = "appointments"
+
+    # Мастер + Время должны быть уникальны
+    __table_args__ = (
+        UniqueConstraint("executor_id", "start_time", name="appointment_uniq"),
+    )
+
+    client_id: Mapped[int] = mapped_column(
+        ForeignKey("clients.id"),
+        nullable=False,
+    )
+
+    executor_id: Mapped[int] = mapped_column(
+        ForeignKey("executors.id"),
+        nullable=False,
+    )
+
+    service_id: Mapped[int] = mapped_column(
+        ForeignKey("services.id"),
+        nullable=False,
+    )
+
+    start_time: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+    )
+
+    end_time: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+    )
+
+    status: Mapped[str] = mapped_column(
+        Enum(AppointmentStatus),
+        default="planned",
+    )
+
+    client: Mapped["Client"] = relationship(
+        back_populates="appointments",
+        lazy="select",
+        uselist=False,
+    )
+
+    executor: Mapped["Executor"] = relationship(
+        back_populates="appointments",
+        lazy="select",
+        uselist=False,
+    )
