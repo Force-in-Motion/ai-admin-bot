f34600dbd307d6071e667e4792c15ef1f004de3d
Force-in-Motion <canzdao@gmail.com>
2026-02-09 16:52:36 +0300

    feat (models): add init  module

---
+++ app/database/models/__init__.py
@@ -0,0 +1,18 @@
+from app.database.models.base import Base
+from app.database.models.service import Service
+from app.database.models.client import Client
+from app.database.models.executor import Executor
+from app.database.models.mixin import UserDataMixin
+from app.database.models.mixin import TimestampMixin
+from app.database.models.appointment import Appointment
+
+__all__ = [
+    "Base",
+    "Client",
+    "Service",
+    "Executor",
+    "Appointment",
+    "UserDataMixin",
+    "TimestampMixin",
+]
+
