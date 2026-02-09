4589576c92a8fa3186ee6f479f9ba4a89ecfab03
Force-in-Motion <canzdao@gmail.com>
2026-02-09 13:20:30 +0300

    create (docker-compose): add settings services

---
+++ app/core/config.py
@@ -0,0 +1,23 @@
+from pydantic import ConfigDict
+from pydantic_settings import BaseSettings
+
+
+class DBSettings(BaseSettings):
+
+    url: str
+
+    cache_url: str
+
+    broker_url: str
+
+    backend_url: str
+
+    echo: bool
+
+    model_config = ConfigDict(
+        env_file=".env",
+        extra="ignore",
+        env_prefix="DB_",
+    )
+
+
